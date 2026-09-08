bd4bae1641be044b837aeb1eeaf50c143750686b
Cosmin Nichita <95240438+xCosmin14@users.noreply.github.com>
2026-09-08 13:53:48 +0300

    Add files via upload

---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit_Release/Calculator_qmllint_module.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculator_qmllint_module"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Calculator_qmllint_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit_Release/Calculator_qmltyperegistration.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculator_qmltyperegistration"
+  "Calculator/Calculator.qmltypes"
+  "Calculator_autogen/mocs_compilation.cpp"
+  "Calculator_autogen/timestamp"
+  "calculator_qmltyperegistrations.cpp"
+  "meta_types/Calculator_json_file_list.txt"
+  "meta_types/Calculator_json_file_list.txt.timestamp"
+  "meta_types/qt6calculator_metatypes.json"
+  "meta_types/qt6calculator_metatypes.json.gen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Calculator_qmltyperegistration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
